9dc22a9cbe457150102459f4d79382b3c0b4ab28
CodeDasg <dsanabriagomez@outlook.es>
2023-08-17 13:02:49 -0500

    Initial commit

---
+++ ejercicios/punto_01_02.py
@@ -0,0 +1,9 @@
+class Student:
+    name: str
+    age: int
+    grades: []
+
+    def __init__(self, name: str, age: int, grades: []):
+        self.name: str = name
+        self.age: int = age
+        self.grades: [] = grades
